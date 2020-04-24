98c3f015dbbe8d7a2e080d29ce580e50145a2053
ariesunique <ariesunique@gmail.com>
2020-04-24 11:16:49 -0400

    Removed unused code

--- frontend/src/components/Points.js
+++ frontend/src/components/Points.js
@@ -15,37 +15,6 @@ class Points extends Component {
     }
   }
 
-  componentDidMount(){
-  }
-
-
-  submitQuestion = (event) => {
-    event.preventDefault();
-    $.ajax({
-      url: '/questions', //TODO: update request URL
-      type: "POST",
-      dataType: 'json',
-      contentType: 'application/json',
-      data: JSON.stringify({
-        question: this.state.question,
-        answer: this.state.answer,
-        difficulty: this.state.difficulty,
-        category: this.state.category
-      }),
-      xhrFields: {
-        withCredentials: true
-      },
-      crossDomain: true,
-      success: (result) => {
-        document.getElementById("add-question-form").reset();
-        return;
-      },
-      error: (error) => {
-        alert('Unable to add question. Please try your request again')
-        return;
-      }
-    })
-  }
 
   handleChange = (event) => {
     this.setState({[event.target.name]: event.target.value})
